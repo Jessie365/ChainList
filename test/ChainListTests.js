5ef2b95b53bec831e121ed3c5d392ef3c8872012
Jessie365 <tsvetani@gmail.com>
2018-04-10 22:17:19 +0300

    Add unit test for raising the event log

--- test/ChainListTests.js
+++ test/ChainListTests.js
@@ -23,16 +23,29 @@ contract('ChainList', function(accounts) {
 
     it("should sell an item", function() {
         return ChainList.deployed().then(function (instance) {
-            ChainListInstace = instance;
-            return ChainListInstace.sellItem(itemToSell, description, web3.toWei(price, "ether"), { from: sellerAddress })
+            ChainListInstance = instance;
+            return ChainListInstance.sellItem(itemToSell, description, web3.toWei(price, "ether"), { from: sellerAddress })
         }).then(function() {
-            return ChainListInstace.getItem();
+            return ChainListInstance.getItem();
         }).then(function (data) {
             assert.equal(data[0], sellerAddress, "Seller address should be " + sellerAddress);
             assert.equal(data[1], itemToSell, "Item should be " + itemToSell);
             assert.equal(data[2], description, "Description should be " + description);
             assert.equal(data[3].toNumber(), web3.toWei(price, "ether"), "Price should be " + web3.toWei(price, "ether"));
         });
+    });
+
+    it("should raise log event when new item is sold", function() {
+        return ChainList.deployed().then(function (instance) {
+            ChainListInstance = instance;
+            return ChainListInstance.sellItem(itemToSell, description, web3.toWei(price, "ether"), { from: sellerAddress })
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, "One log should be submited when selling an item");
+            assert.equal(receipt.logs[0].event, "LogSellItem", "The raised event should be LogSellItem");
+            assert.equal(receipt.logs[0].args._sellerAddress, sellerAddress, "Seller address should be " + sellerAddress);
+            assert.equal(receipt.logs[0].args._itemToSell, itemToSell, "Item should be " + itemToSell);
+            assert.equal(receipt.logs[0].args._price, web3.toWei(price, "ether"), "Price should be " + web3.toWei(price, "ether"));
+        })
     });
 
 });
